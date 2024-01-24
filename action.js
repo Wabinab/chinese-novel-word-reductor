8764ac6c756f1a794496a4795dd309b5bbda0327
Wabinab <chowjunwei37@gmail.com>
2024-01-24 02:17:12 +0000

    test again

---
+++ action.js
@@ -0,0 +1,3 @@
+console.log("called action");
+var x = document.getElementsByClassName("txtnav")[0].innerText;
+console.warn("Inner side: " + x);
--- background.js
+++ background.js
@@ -1,7 +1,18 @@
 const browser = "https://www.69xinshu.com/txt/";
 
-chrome.runtime.onInstalled.addListener(async (tab) => {
+// chrome.runtime.onInstalled.addListener(async (tab) => {
+//     console.log(tab.url);
+//     var x = document.getElementsByClassName("txtnav")[0].innerText;
+//     console.warn(x);
+// });
+var x = document.getElementsByClassName("txtnav")[0].innerText;
+console.warn(x);
+
+chrome.action.onClicked.addListener(async (tab) => {
     console.log(tab.url);
-    var x = document.getElementsByClassName("txtnav")[0].innerText;
-    console.warn(x);
-});
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['action.js']
+    });
+})
