068aaab995d217f67b3be51374f4d497af2c334d
Wabinab <chowjunwei37@gmail.com>
2024-01-24 09:55:35 +0000

    ok correct filtering now

--- content-script.js
+++ content-script.js
@@ -13,18 +13,19 @@ try {
     remnants = remnants.map(c => c.split('\n').flat()).flat();
     remnants = remnants.filter(c => c != '');  // not empty
     var first_line = mainhtml.split(remnants[1])[0];
-    var last_line = mainhtml.split(remnants[remnants.length-1])[1];
+    var last_remnant = remnants[remnants.length-1];
+    var last_line = mainhtml.split(last_remnant)[1];
 
     // Filter now
     // We'll allow length definition later. 
     // For now, let's fix it to 50.
-    remnants = remnants.filter(x => { x.length >= 47 
-        || !x.trim().startsWith("【")
-        || !x.includes('"')
-    });
+    remnants = remnants.filter(x =>  x.length >= 47 
+        || x.trim().startsWith("【")
+        || x.includes('"')
+    );
 
     // Join back
-    var final_html = `${first_line}${remnants.join('\n<br><br>\n')}\n<br>\n${last_line}`;
+    var final_html = `${first_line}${remnants.join('\n<br><br>\n')}\n<br>\n${last_remnant}\n<br>\n${last_line}`;
 
     document.getElementsByClassName("txtnav")[0].innerHTML = final_html;
 } catch (e) {
