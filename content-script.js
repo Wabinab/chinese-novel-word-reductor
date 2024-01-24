51ef103819f8bbd7109ffb846a564d06d8c8c088
Wabinab <chowjunwei37@gmail.com>
2024-01-24 02:58:57 +0000

    ok basic done. Test

--- content-script.js
+++ content-script.js
@@ -1,2 +1,25 @@
+// Note, since there are title that we can't simply remove, the first line will 
+// always be not removed, regardless of its length. 
 var maintext = document.getElementsByClassName("txtnav")[0].innerText;
-console.log(maintext);
+var mainhtml = document.getElementsByClassName("txtnav")[0].innerHTML;
+
+// Split by \n\n (double)
+var paragraphs = maintext.split("\n\n");
+var first_line = mainhtml.split(paragraphs[1])[0];
+// var first_line = paragraphs[0];
+
+// Some isn't split by \n\n, so we'll solve that. 
+var remnants = paragraphs.slice(1)
+remnants = remnants.map(c => c.split('\n').flat()).flat();
+remnants = remnants.filter(c => c != '');  // not empty
+var last_line = mainhtml.split(remnants[remnants.length-1])[1];
+
+// Filter now
+// We'll allow length definition later. 
+// For now, let's fix it to 50.
+remnants = remnants.filter(x => x.length >= 50);
+
+// Join back
+var final_html = `${first_line}\n<br><br>\n${remnants.join('\n<br><br>\n')}`;
+
+document.getElementsByClassName("txtnav")[0].innerHTML = final_html;
