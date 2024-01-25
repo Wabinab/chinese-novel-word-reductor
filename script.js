0a687f3938be5fb13e165da1eb4fb30332c0e1a9
Wabinab <chowjunwei37@gmail.com>
2024-01-25 03:33:51 +0000

    test persistence

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const activate = document.getElementById('activate');
-activate.addEventListener('click', async () => {
-    console.log("Checked: ", activate.checked);
-    await chrome.scripting.unregisterContentScripts({ 
-        ids: ["basic-script"] 
-    })
-    .then(() => console.log("un-registration complete"));
+activate.addEventListener('click', () => {
+    if (activate.checked) { register_script(); }
+    else { unregister_script(); }
 });
 
 
 function register_script() {
-    console.log("register script fn called.");
+    chrome.scripting.registerContentScripts([{
+        id: "basic-script",
+        js: ["content-script.js"],
+        persistAcrossSessions: true,
+        matches: ["https://www.69xinshu.com/txt/*"],
+    }])
+    .then(() => console.log("registration complete"))
+    .catch((err) => console.warn("unexpected error during registration", err));
+}
+
+function unregister_script() {
+    chrome.scripting.unregisterContentScripts({ 
+        ids: ["basic-script"] 
+    })
+    .then(() => console.log("un-registration complete"));
 }
