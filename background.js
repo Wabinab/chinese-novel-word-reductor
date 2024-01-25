46c5d46d0fd52eec05d5ae78376f7070dfbd4906
Wabinab <chowjunwei37@gmail.com>
2024-01-25 05:01:39 +0000

    ok whatever

--- background.js
+++ background.js
@@ -20,12 +20,34 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.active) {
         chrome.scripting.getRegisteredContentScripts()
         .then(scripts => {
-            if (scripts.find(c => c.id == "basic-script") && tab.url.includes("https://www.69xinshu.com/txt/")) {
-                chrome.scripting.executeScript({
-                    target: {tabId: tabId},
-                    files: ["content-script.js"]
-                });
+            if (scripts.find(c => c.id == "basic-script")) {
+                unregister_script();
+                register_script();
             }
         });
     }
 });
+
+function register_script() {
+    chrome.scripting.registerContentScripts([{
+        id: "basic-script",
+        js: ["content-script.js"],
+        persistAcrossSessions: true,
+        matches: ["https://www.69xinshu.com/txt/*"],
+    }])
+    .then(() => {
+        console.log("registration complete");
+        chrome.storage.local.set({ "basic-script": true })
+    })
+    .catch((err) => console.warn("unexpected error during registration", err));
+}
+
+function unregister_script() {
+    chrome.scripting.unregisterContentScripts({ 
+        ids: ["basic-script"] 
+    })
+    .then(() => { 
+        console.log("un-registration complete");
+        chrome.storage.local.set({ "basic-script": false })
+    });
+}
