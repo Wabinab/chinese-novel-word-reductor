7f31cca0cf8a2e0ba9d5f003f045521d55d77e61
Wabinab <chowjunwei37@gmail.com>
2024-01-25 03:17:31 +0000

    test now changes

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-// const button = document.getElementById('flexSwitchCheckChecked');
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.scripting.registerContentScripts([{
+        id: "basic-script",
+        js: ["content-script.js"],
+        persistentAcrossSessions: true,
+        matches: ["https://www.69xinshu.com/txt/*"],
+    }])
+    .then(() => console.log("registration complete"))
+    .catch((err) => console.warn("unexpected error during registration", err));
+  });
 
-function activate() {
-    console.log("test activate runs");
+const activate = document.getElementById('activate');
+activate.addEventListener('click', async () => {
+    console.log("clicked activate.");
+    register_script();
+    await chrome.scripting.unregisterContentScripts({ 
+        ids: ["basic-script"] 
+    })
+    .then(() => console.log("un-registration complete"));
+});
+
+
+function register_script() {
+    console.log("register script fn called.");
 }
