de23d955314c5a5040801973b50cf670e2e0579d
Wabinab <chowjunwei37@gmail.com>
2024-02-18 04:25:52 +0000

    update with lwxs

---
+++ dynamic-extension/content_scripts/lwxs_bg.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  document.body.style.background = "#f2f3f4";
+  document.getElementsByClassName('box_con')[0].style.backgroundColor = "#ccdfcd";
+  document.getElementById('content').innerHTML = document.getElementById('content').innerHTML.replaceAll('</p><p>', '</p><br><p>');
+}, 1500);
