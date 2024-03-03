76ab8e1f1b6485e4b4252c5b4b49bbbe7d8c65cc
Wabinab <chowjunwei37@gmail.com>
2024-03-03 20:22:25 +0800

    change timeout for ddxs

--- dynamic-extension/content_scripts/ddxs_bg.js
+++ dynamic-extension/content_scripts/ddxs_bg.js
@@ -2,4 +2,4 @@ setTimeout(() => {
     document.body.style.backgroundColor = "#f2f3f4";
 document.getElementById('amain').style.backgroundColor = "#ccdfcd";
 if (window.screen.width >= 540) document.getElementById('contents').style.fontSize = '32px';
-}, 1500);
+}, 400);
