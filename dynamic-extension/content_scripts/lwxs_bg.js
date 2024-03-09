473e130057897adc51cf78dacfda4544ecd3e876
Wabinab <chowjunwei37@gmail.com>
2024-03-09 09:51:56 +0000

    lwxs now filter out short sentences no speech.

--- dynamic-extension/content_scripts/lwxs_bg.js
+++ dynamic-extension/content_scripts/lwxs_bg.js
@@ -3,6 +3,15 @@ setTimeout(() => {
   document.getElementsByClassName('box_con')[0].style.backgroundColor = "#ccdfcd";
   document.getElementById('content').innerHTML = document.getElementById('content').innerHTML.replaceAll('</p><p>', '</p><br><p>');
 
+  const content = document.getElementById('content');
+  const text = content.innerText.split('\n\n');
+  const spch_list = text.filter(x =>  x.trim().length >= 40
+      || x.trim().startsWith("【")
+      || (x.includes('“') && x.trim().length >= 10)
+  );
+  const scph_list2 = spch_list.map(c => '<p>' + c.trimStart('\n'));
+  content.innerHTML = scph_list2.join('</p><br><p>');
+
   // Set next page button
   let prev_href = getElementsByText('上一章', 'a')[0].href;
   let next_href = getElementsByText('下一章', 'a')[0].href;
