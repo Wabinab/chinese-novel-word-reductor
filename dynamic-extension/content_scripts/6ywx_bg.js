06f01a57eb8741f790f1184440ab6d8809d4e29d
Wabinab <chowjunwei37@gmail.com>
2024-03-09 04:45:41 +0000

    6ywx done

---
+++ dynamic-extension/content_scripts/6ywx_bg.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+  main();
+}, 100);
+
+function main() {
+  document.body.style.background = "#f2f3f4";
+  document.getElementsByClassName('content')[0].style.backgroundColor = "#ccdfcd";
+  document.getElementById('content').style.fontSize = '32px';
+
+  // Break point 40 + speech, by default. 
+  const content = document.getElementById('content');
+  const text = content.innerText.split('\n\n');
+  const spch_list = text.filter(x =>  x.length >= 40
+      || x.trim().startsWith("【")
+      || (x.includes('“') && x.length >= 10)
+  );
+  content.innerText = spch_list.join('\n\n') + '\n\n';
+}
