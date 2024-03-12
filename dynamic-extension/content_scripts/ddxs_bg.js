aebb181219475e2185f22cf8aeca0c26bd45fba7
Wabinab <chowjunwei37@gmail.com>
2024-03-12 09:00:28 +0800

    yes

--- dynamic-extension/content_scripts/ddxs_bg.js
+++ dynamic-extension/content_scripts/ddxs_bg.js
@@ -1,5 +1,7 @@
 setTimeout(() => {
-    document.body.style.backgroundColor = "#f2f3f4";
-document.getElementById('amain').style.backgroundColor = "#ccdfcd";
-if (window.screen.width >= 540) document.getElementById('contents').style.fontSize = '32px';
+  document.body.style.backgroundColor = "#f2f3f4";
+  document.getElementById('amain').style.backgroundColor = "#ccdfcd";
+  if (window.screen.width >= 540) document.getElementById('contents').style.fontSize = '32px';
+
+  document.getElementById('contents').innerHTML = document.getElementById('contents').innerHTML.replaceAll('<p>', '<p style="letter-spacing: 1px">');
 }, 400);
